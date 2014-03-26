886b7c76d2f8abdf1c7ca7999dc2f251ec241af9
jeremyBass <jeremybass@cableone.net>
2014-03-26 15:13:41 -0700

    changes filter for action

--- connections-education/includes/class-connections-education.php
+++ connections-education/includes/class-connections-education.php
@@ -17,7 +17,7 @@ if (!class_exists('Connections_Education')) {
 				// Since we're using a custom field, we need to add our own sanitization method.
 				add_filter( 'cn_meta_sanitize_field-entry_education', array( __CLASS__, 'sanitize') );
 				add_filter( 'cncsv_map_import_fields', array( __CLASS__, 'map_import_fields' ));
-				add_filter( 'cncsv_import_fields', array( __CLASS__, 'import_fields' ), 10, 2);
+				add_action( 'cncsv_import_fields', array($this, 'import_fields' ),10,2);
             }
 			// Register the metabox and fields.
 			add_action( 'cn_metabox', array( __CLASS__, 'registerMetabox') );
@@ -56,7 +56,7 @@ if (!class_exists('Connections_Education')) {
 			$fields['cneducation_schoolid'] = 'Education | School ID';
 			return $fields;
 		}
-		public static function import_fields( $entryId, $row ){
+		public  function import_fields( $entryId, $row ){
 			$tmp=array(
 				'degree'=>'',
 				'year'=>'',
@@ -71,13 +71,12 @@ if (!class_exists('Connections_Education')) {
 			if( isset($row->cneducation_schoolid) ){
 				$tmp['schoolid'] = $row->cneducation_schoolid;
 			}
-			cnEntry_Action::meta('update', $entryId, array(
+			cnEntry_Action::meta('add', $entryId, array(
 				array(
 					'key' => "cneducation",
 					'value' =>$tmp
 				)
 			));
-			return;
 		}	
 		public static function loadTextdomain() {
 
